c5748389f964c9d0e6e7e3bb6ba8adf73a73da9c
laureyes155 <lreyeso1001@gmail.com>
2024-11-01 16:49:31 -0600

    Update CaluladoraImpl.java

--- src/main/java/calculadora/CaluladoraImpl.java
+++ src/main/java/calculadora/CaluladoraImpl.java
@@ -4,7 +4,17 @@ public class CaluladoraImpl implements Calculadora {
 	
 	@Override
 	public int calcular(int i1, char op, int i2) {
-		return i1+i2;
+		
+		switch(op){
+			case '+':
+				return i1+i2;
+			case '-':
+				return i1-i2;
+			default:
+				return 0;
+				
+		}
+		
 	}
 
 }
